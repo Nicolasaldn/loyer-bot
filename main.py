48988114e96daf62e79354524c5540d986357857
Nicolasaldn <133792724+Nicolasaldn@users.noreply.github.com>
2025-05-05 21:32:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Loyer bot en ligne. Prêt à envoyer les rappels.")
+
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+message = "📢 Ceci est un test automatique depuis Render et Google Sheets."
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+payload = {
+    "chat_id": CHAT_ID,
+    "text": message
+}
+
+response = requests.post(url, data=payload)
+print("Message envoyé:", response.status_code, response.text)
