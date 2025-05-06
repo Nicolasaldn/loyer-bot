ee43891402041119e3d6eda5727710b4f74d4005
Nicolasaldn <133792724+Nicolasaldn@users.noreply.github.com>
2025-05-06 10:47:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import os
+import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+print("🧪 Test Auth Google Sheets")
+
 try:
-    spreadsheet = client.open_by_key(SHEET_ID)
-    print(f"✅ Fichier ouvert : {spreadsheet.title}")
+    raw_creds = os.getenv("GOOGLE_SHEET_CREDENTIALS_JSON")
+    print("🔍 JSON trouvé ?", raw_creds is not None)
+
+    creds_dict = json.loads(raw_creds)
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(creds)
+    print("✅ Authentification réussie.")
 except Exception as e:
-    print(f"❌ Erreur à l'ouverture du fichier : {e}")
+    print("❌ Erreur d’authentification :", e)
