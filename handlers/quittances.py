c03360b1564df83d7839dd6fd2aa7aa36ee2a5a9
Nicolasaldn <133792724+Nicolasaldn@users.noreply.github.com>
2025-05-06 17:22:10 +0200

    Delete handlers directory

--- handlers/quittances.py
+++
@@ -1,5 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def handle_quittance(update: Update, context: ContextTypes.DEFAULT_TYPE, *args):
-    await update.message.reply_text("📄 Fonction quittance pas encore implémentée.")
