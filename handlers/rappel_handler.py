f0d85547c62dc4d7204b137b7fb8d175edf3b0ad
Nicolasaldn <133792724+Nicolasaldn@users.noreply.github.com>
2025-05-24 15:02:59 +0200

    Update rappel_handler.py

--- handlers/rappel_handler.py
+++ handlers/rappel_handler.py
@@ -23,11 +23,11 @@ def handle_rappel_command(update: Update, context: CallbackContext):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     query = update.callback_query
-query.answer()
-query.edit_message_text(
-    text="Quel locataire pour le rappel ?",
-    reply_markup=reply_markup
-)
+    query.answer()
+    query.edit_message_text(
+        text="Quel locataire pour le rappel ?",
+        reply_markup=reply_markup
+    )
     print("✅ [DEBUG] Commande rappel déclenchée.")
     return SELECT_TENANT
 
